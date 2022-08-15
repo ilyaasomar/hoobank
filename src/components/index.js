31c7c742a35af820f48805a7ea8a8d8c42f77c7a
unknown <ilyaascumar10@gmail.com>
2022-08-15 21:06:53 +0300

    stoped 49min

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Business from './Business';
+import Button from './Button';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Testimonials from './Testimonials';
+import Stats from './Stats';
+
+export {Navbar,Hero,Business,Button,Billing,CardDeal,Clients,CTA,FeedbackCard,
+    Footer,GetStarted,Testimonials,Stats};
+
